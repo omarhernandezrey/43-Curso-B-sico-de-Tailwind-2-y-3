231900c7aaf5e5c8660b39c86a3177474e92c7ea
OmarHernandezRey <hernandezreyomar@gmail.com>
2024-08-09 10:43:17 -0500

    5Creación del proyecto e instalación de Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Ajusta estas rutas a tus archivos reales
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

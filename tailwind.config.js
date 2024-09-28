3e77b5de70f6adf6b5b76b7f766e5fdc1ee72fd3
omarhernandezrey <hernandezreyomar@gmail.com>
2024-09-28 12:46:26 -0500

    12Tailwind como API para la creación de un Design System

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js}', // Ajusta estas rutas a tus archivos reales
-    './public/**/*.html'
-  ],
+  content: ["./public/**/*.html", "./src/**/*.{html,js}"], // Asegura que Tailwind analice todos los archivos necesarios
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+      },
+      backgroundColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      },
+      textColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
